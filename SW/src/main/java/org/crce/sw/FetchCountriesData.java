ba27eed078c1908e861d3a6b6645f2ddabdd2da0
Leon D'souza <LeonDsouza@users.noreply.github.com>
2017-01-22 19:52:12 +0530

    Update FetchCountriesData.java

--- SW/src/main/java/org/crce/sw/FetchCountriesData.java
+++ SW/src/main/java/org/crce/sw/FetchCountriesData.java
@@ -5,6 +5,11 @@
  */
 package org.crce.sw;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 import org.apache.jena.query.ParameterizedSparqlString;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
@@ -17,14 +22,17 @@ import org.apache.jena.query.ResultSetFormatter;
  */
 public class FetchCountriesData {
 
-    public void getCountriesInformation() {
+    public void getCountriesInformation() throws FileNotFoundException, IOException {
         ParameterizedSparqlString qs = new ParameterizedSparqlString("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n "
-                + "SELECT * {?Subject rdf:type <http://dbpedia.org/ontology/Country> . ?Subject ?Predicate ?Object } ORDER BY ?Subject ");
+                + "SELECT * {     ?Subject rdf:type <http://dbpedia.org/ontology/Country> .     ?Subject ?Predicate ?Object } ORDER BY ?Subject LIMIT 10000 OFFSET 10000");
 
         QueryExecution exec = QueryExecutionFactory.sparqlService("https://dbpedia.org/sparql", qs.asQuery());
         //exec.setTimeout(10000000);
-        exec.setTimeout(-123);
+        exec.setTimeout(10, TimeUnit.MINUTES);
         ResultSet results = exec.execSelect();
+        ResultSetFormatter.outputAsCSV(new FileOutputStream(new File("C:/Users/leons/Desktop/CountryData.csv")), results);
         ResultSetFormatter.out(results);
+        CSVCountryNameFetcher countryNameFetcher = new CSVCountryNameFetcher();
+        countryNameFetcher.fetchDataFromCSV();
     }
 }
